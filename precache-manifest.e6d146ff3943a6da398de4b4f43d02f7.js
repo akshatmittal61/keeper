77a74ee1f57e363948ee9e32a907c66c250cd72c
akshatmittal61 <akshatmittal2506@gmail.com>
2021-10-11 18:52:34 +0530

    Updates

---
+++ precache-manifest.e6d146ff3943a6da398de4b4f43d02f7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13211290877ec2743699de82cbdc2e5",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "a74ce3945f7ba978ec68",
+    "url": "/keeper/static/js/2.98b330b3.chunk.js"
+  },
+  {
+    "revision": "fecc4704258fee012cbe",
+    "url": "/keeper/static/js/main.33c4b935.chunk.js"
+  },
+  {
+    "revision": "af4a55ea2c5d08d39996",
+    "url": "/keeper/static/js/runtime-main.f5c12d9a.js"
+  }
+]);
